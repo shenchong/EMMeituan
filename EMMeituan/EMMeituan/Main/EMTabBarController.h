9b44483d9b1d82af0d25866088d068634cbbc0a9
shenchong <569471293@qq.com>
2016-02-24 19:01:31 +0800

    自定义tabBar初步

---
+++ EMMeituan/EMMeituan/Main/EMTabBarController.h
@@ -0,0 +1,13 @@
+//
+//  EMTabBarController.h
+//  EMMeituan
+//
+//  Created by easemob on 16/2/24.
+//  Copyright © 2016年 easemob. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EMTabBarController : UITabBarController
+
+@end
---
+++ EMMeituan/EMMeituan/Main/EMTabBarController.m
@@ -0,0 +1,37 @@
+//
+//  EMTabBarController.m
+//  EMMeituan
+//
+//  Created by easemob on 16/2/24.
+//  Copyright © 2016年 easemob. All rights reserved.
+//
+
+#import "EMTabBarController.h"
+
+@interface EMTabBarController ()
+
+@end
+
+@implementation EMTabBarController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
