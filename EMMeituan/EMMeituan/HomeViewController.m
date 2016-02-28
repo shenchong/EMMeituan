1498b6493d53296ad2e4680dc07c657139fdab9d
心忆年华 <mazhihua5152570@126.com>
2016-02-28 16:44:27 +0800

    在自定义导航栏中添加搜索框

--- EMMeituan/EMMeituan/HomeViewController.m
+++ EMMeituan/EMMeituan/HomeViewController.m
@@ -33,8 +33,7 @@
 {
     ///创建导航栏
     UIView * navView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 64)];
-#warning TODO //修改nav颜色
-    navView.backgroundColor = [UIColor redColor];
+    navView.backgroundColor = My_Color(33, 192, 174);
     [self.view addSubview:navView];
     ///1 选择城市
     //城市
@@ -57,8 +56,25 @@
     
     ///3 搜索框
     //搜索框
+    UIButton * searchView = [[UIButton alloc] initWithFrame:CGRectMake(CGRectGetMaxX(cityBtn.frame)+21, 28, CGRectGetMinX(mapBtn.frame)-21-CGRectGetMaxX(cityBtn.frame), 30)];
+    searchView.backgroundColor = [UIColor whiteColor];
+    searchView.layer.masksToBounds = YES;
+    searchView.layer.cornerRadius = 14;
+    [searchView addTarget:self action:@selector(searchViewButtonClick:) forControlEvents:UIControlEventTouchUpInside];
+    [navView addSubview:searchView];
     
     //搜索图片
+    UIImageView * searchImage = [[UIImageView alloc] initWithFrame:CGRectMake(5, 7, 15, 15)];
+    searchImage.image = [UIImage imageNamed:@"icon_homepage_search"];
+    [searchView addSubview:searchImage];
+    
+    //站位文字
+    UILabel * placeHolerLabel = [[UILabel alloc] initWithFrame:CGRectMake(22, 0, CGRectGetWidth(searchView.frame)-22, 30)];
+    placeHolerLabel.text = @"主人想搜什么，点这里哦";
+    placeHolerLabel.font = [UIFont systemFontOfSize:14];
+    placeHolerLabel.textColor = [UIColor grayColor];
+    [searchView addSubview:placeHolerLabel];
+    
     
 }
 
@@ -72,6 +88,11 @@
 -(void)mapButtonClick:(UIButton *)mapBtn
 {
     NSLog(@"主人对这个城市不熟悉，要查一下具体位置啊");
+}
+
+-(void)searchViewButtonClick:(UIButton *)searchView
+{
+    NSLog(@"主人想要搜索，热门服务快点过来啊！");
 }
 
 - (void)didReceiveMemoryWarning {
