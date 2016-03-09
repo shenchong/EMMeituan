ecbd2c1b94c4ed1b777d91242d46e1060155b3e4
心忆年华 <mazhihua5152570@126.com>
2016-03-09 20:09:31 +0800

    设置购物商城

--- EMMeituan/EMMeituan/Home/Views/ShopView/EMShopCell.m
+++ EMMeituan/EMMeituan/Home/Views/ShopView/EMShopCell.m
@@ -8,10 +8,53 @@
 
 #import "EMShopCell.h"
 
+#define HM_IMAGE_COUNT 5
+
+
+@interface EMShopCell()
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+
+@end
 @implementation EMShopCell
 
 - (void)awakeFromNib {
     // Initialization code
+    
+    // 1. 添加5张图片
+    for (int i = 0; i < HM_IMAGE_COUNT; i++) {
+        // 1. 创建UIImageView
+        UIImageView *imageView = [[UIImageView alloc] init];
+        
+        // 2. 添加到scrollView
+        [self.scrollView addSubview:imageView];
+        
+        // 3. 设置frame
+        CGFloat imageY = 0;
+        CGFloat imageW = 110;
+        CGFloat imageH = 110;
+        CGFloat martin = 5;
+        CGFloat imageX = (imageW + martin)* i;
+        imageView.frame = CGRectMake(imageX, imageY, imageW, imageH);
+        
+        // 4. 设置图片
+        NSString *name = [NSString stringWithFormat:@"img_%02d", i+1];
+        imageView.image = [UIImage imageNamed:name];
+    }
+    
+    // 2. 设置scrollView的滚动范围
+    CGSize contentSize = CGSizeMake((110 + 5) * HM_IMAGE_COUNT, 0);
+    self.scrollView.contentSize =  contentSize;
+    
+    // 3. 不显示滚动条
+    self.scrollView.showsHorizontalScrollIndicator = NO;
+    
+    // 4. 分页
+    // 分页的原理:scrollView内部会以scrollView.frame.size.wigth的宽度为一个单位, 对scrollView内部需要展示的所有内容进行分割
+//    self.scrollView.pagingEnabled = YES;
+    
+//    // 5. 设置页码
+//    self.pageControl.numberOfPages = 5;
+
 }
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
