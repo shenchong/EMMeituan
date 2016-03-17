ee43caaba9a3a69c44e5876159227551320d5e86
as278257357 <as278257357@163.com>
2016-03-17 16:32:40 +0800

    DropDown

--- EMMeituan/EMMeituan/Merchant/EMMerchantViewController.m
+++ EMMeituan/EMMeituan/Merchant/EMMerchantViewController.m
@@ -9,8 +9,20 @@
 #import "EMMerchantViewController.h"
 #import "EMMerchantCell.h"
 #import "EMMerchantModel.h"
+#import "JSDropDownMenu.h"
 
-@interface EMMerchantViewController ()<UITableViewDelegate,UITableViewDataSource>
+@interface EMMerchantViewController ()<UITableViewDelegate,UITableViewDataSource,JSDropDownMenuDataSource,JSDropDownMenuDelegate>
+
+{
+    NSMutableArray *_data1;
+    NSMutableArray *_data2;
+    NSMutableArray *_data3;
+    
+    NSInteger _currentData1Index;
+    NSInteger _currentData2Index;
+    NSInteger _currentData3Index;
+}
+
 @property (nonatomic, strong) UITableView *tableView;
 @property (nonatomic, strong) NSMutableArray *merchantListSource;
 @end
@@ -88,38 +100,21 @@
 }
 
 -(void)initViews{
-    //筛选
-    UIView *filterView = [[UIView alloc] initWithFrame:CGRectMake(0, 64, SCREEN_WIDTH, 40)];
-    filterView.backgroundColor = [UIColor whiteColor];
-    [self.view addSubview:filterView];
     
-    NSArray *filterName = @[@"全部",@"全部",@"智能排序"];
-    //筛选
-    for (int i = 0; i < 3; i++) {
-        //文字
-        UIButton *filterBtn = [UIButton buttonWithType:UIButtonTypeCustom];
-        filterBtn.frame = CGRectMake(i*SCREEN_WIDTH/3, 0, SCREEN_WIDTH/3-15, 40);
-        filterBtn.tag = 100+i;
-        filterBtn.titleLabel.font = [UIFont systemFontOfSize:13];
-        [filterBtn setTitle:filterName[i] forState:UIControlStateNormal];
-        [filterBtn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-        [filterBtn setTitleColor:My_Color(33, 192, 174) forState:UIControlStateSelected];
-//        [filterBtn addTarget:self action:@selector(OnFilterBtn:) forControlEvents:UIControlEventTouchUpInside];
-        [filterView addSubview:filterBtn];
-        
-        //三角
-        UIButton *triangleBtn = [UIButton buttonWithType:UIButtonTypeCustom];
-        triangleBtn.frame = CGRectMake((i+1)*SCREEN_WIDTH/3-15, 16, 8, 7);
-        triangleBtn.tag = 120+i;
-        [triangleBtn setImage:[UIImage imageNamed:@"icon_arrow_dropdown_normal"] forState:UIControlStateNormal];
-        [triangleBtn setImage:[UIImage imageNamed:@"icon_arrow_dropdown_selected"] forState:UIControlStateSelected];
-        [filterView addSubview:triangleBtn];
-    }
+    NSArray *food = @[@"全部", @"火锅", @"川菜", @"西餐", @"自助餐"];
+    NSArray *travel = @[@"全部", @"周边游", @"景点门票", @"国内游", @"境外游"];
     
-    //下划线
-    UIView *lineView = [[UIView alloc] initWithFrame:CGRectMake(0, 39.5, SCREEN_WIDTH, 0.5)];
-    lineView.backgroundColor = My_Color(192, 192, 192);
-    [filterView addSubview:lineView];
+    _data1 = [NSMutableArray arrayWithObjects:@{@"title":@"美食", @"data":food}, @{@"title":@"旅游", @"data":travel}, nil];
+    _data2 = [NSMutableArray arrayWithObjects:@"智能排序", @"离我最近", @"评价最高", @"最新发布", @"人气最高", @"价格最低", @"价格最高", nil];
+    _data3 = [NSMutableArray arrayWithObjects:@"智能排序", @"单人餐", @"双人餐", @"3~4人餐", nil];
+    JSDropDownMenu *menu = [[JSDropDownMenu alloc] initWithOrigin:CGPointMake(0, 65) andHeight:45];
+    menu.indicatorColor = [UIColor colorWithRed:175.0f/255.0f green:175.0f/255.0f blue:175.0f/255.0f alpha:1.0];
+    menu.separatorColor = [UIColor colorWithRed:210.0f/255.0f green:210.0f/255.0f blue:210.0f/255.0f alpha:1.0];
+    menu.textColor = [UIColor colorWithRed:83.f/255.0f green:83.f/255.0f blue:83.f/255.0f alpha:1.0f];
+    menu.dataSource = self;
+    menu.delegate = self;
+    
+    [self.view addSubview:menu];
     
     //tableview
     self.tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 64+40, SCREEN_WIDTH, SCREEN_HEIGHT-64-40-49) style:UITableViewStylePlain];
@@ -199,10 +194,108 @@
     
 }
 
+#pragma JSDropDownMenuDataSource
+- (CGFloat)widthRatioOfLeftColumn:(NSInteger)column {
+    if (column==0) {
+        return 0.3;
+    }
+    
+    return 1;
+}
+
+-(BOOL)haveRightTableViewInColumn:(NSInteger)column{
+    
+    if (column==0) {
+        return YES;
+    }
+    return NO;
+}
+
+- (NSInteger)menu:(JSDropDownMenu *)menu numberOfRowsInColumn:(NSInteger)column leftOrRight:(NSInteger)leftOrRight leftRow:(NSInteger)leftRow{
+    
+    if (column==0) {
+        if (leftOrRight==0) {
+            
+            return _data1.count;
+        } else{
+            
+            NSDictionary *menuDic = [_data1 objectAtIndex:leftRow];
+            return [[menuDic objectForKey:@"data"] count];
+        }
+    } else if (column==1){
+        
+        return _data2.count;
+        
+    } else if (column==2){
+        
+        return _data3.count;
+    }
+    
+    return 0;
+}
+
+- (NSString *)menu:(JSDropDownMenu *)menu titleForColumn:(NSInteger)column{
+    
+    switch (column) {
+        case 0: return [[_data1[0] objectForKey:@"data"] objectAtIndex:0];
+            break;
+        case 1: return _data2[0];
+            break;
+        case 2: return _data3[0];
+            break;
+        default:
+            return nil;
+            break;
+    }
+}
+
+-(NSInteger)currentLeftSelectedRow:(NSInteger)column{
+    
+    if (column==0) {
+        
+        return _currentData1Index;
+        
+    }
+    if (column==1) {
+        
+        return _currentData2Index;
+    }
+    
+    return 0;
+}
+
+- (NSString *)menu:(JSDropDownMenu *)menu titleForRowAtIndexPath:(JSIndexPath *)indexPath {
+    
+    if (indexPath.column==0) {
+        if (indexPath.leftOrRight==0) {
+            NSDictionary *menuDic = [_data1 objectAtIndex:indexPath.row];
+            return [menuDic objectForKey:@"title"];
+        } else{
+            NSInteger leftRow = indexPath.leftRow;
+            NSDictionary *menuDic = [_data1 objectAtIndex:leftRow];
+            return [[menuDic objectForKey:@"data"] objectAtIndex:indexPath.row];
+        }
+    } else if (indexPath.column==1) {
+        
+        return _data2[indexPath.row];
+        
+    } else {
+        
+        return _data3[indexPath.row];
+    }
+}
+
+- (NSInteger)numberOfColumnsInMenu:(JSDropDownMenu *)menu {    
+    return 3;
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+#pragma JSDropDownMenuDelegate
+- (void)menu:(JSDropDownMenu *)menu didSelectRowAtIndexPath:(JSIndexPath *)indexPath {
+    
 }
 
 @end
